2b3c8b7c09f0664b0db0c9fef7d73827b85833bc
jdratnayake <janithadevin@gmail.com>
2023-08-22 21:23:13 +0530

    initialize

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>EMvidence</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+  );
+}
+
+export default App;
