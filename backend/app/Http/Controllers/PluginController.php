7a7750011c1a58977f015c6ed7b0225f338bfbb0
Janitha Ratnayake <62105403+jdratnayake@users.noreply.github.com>
2023-11-29 13:23:45 +0530

    em data analysis backend api initiated

--- backend/app/Http/Controllers/PluginController.php
+++ backend/app/Http/Controllers/PluginController.php
@@ -6,6 +6,19 @@ use Illuminate\Http\Request;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
+function execute_python_script($path, ...$variables)
+{
+    $process = new Process(['python3', $path, ...$variables]);
+
+    try {
+        $process->mustRun();
+        return json_decode($process->getOutput());
+
+    } catch (ProcessFailedException $exception) {
+        return $exception->getMessage();
+    }
+}
+
 class PluginController extends Controller
 {
     public function test()
@@ -25,5 +38,23 @@ class PluginController extends Controller
         }
     }
 
-    // public function
+    public function executeAnalysisPlugin(Request $request)
+    {
+        // Header parameters
+        $emRawFileName = $request->header("em_raw_file_name");
+        $analysisPluginName = $request->header("analysis_plugin_name");
+        $analysisPluginMlModelName = $request->header("analysis_plugin_ml_model_name");
+
+        // Set em preprocessing path
+        $emPreprocessingFileName = explode(".", $emRawFileName)[0] . ".txt";
+        $emPreprocessingFilePath = env("EM_PREPROCESSING_DIRECTORY_PATH") . "/" . $emPreprocessingFileName;
+
+        // Set path variables
+        $analysisPluginPath = env("ANALYSIS_PLUGIN_DIRECTORY_PATH") . "/" . $analysisPluginName;
+        $analysisPluginMlModelPath = env("ML_MODEL_DIRECTORY_PATH") . "/" . $analysisPluginMlModelName;
+
+        $output = execute_python_script($analysisPluginPath, $emPreprocessingFilePath, $analysisPluginMlModelPath);
+
+        return response()->json(["output" => $output]);
+    }
 }
