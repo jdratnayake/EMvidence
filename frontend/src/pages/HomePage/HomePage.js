b928e9946114d238d928f4e00803e4b6d2cc05b6
Janitha Ratnayake <62105403+jdratnayake@users.noreply.github.com>
2024-01-01 21:59:42 +0530

    create a global state using context api

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "./HomePage.css";
+import { useUser } from "../../contexts/UserContext";
 
 function HomePage() {
-  return <div>Home Page</div>;
+  const { user, loginUser } = useUser();
+
+  const checkStatus = () => {
+    console.log(user);
+  };
+  const updateStatus = () => {
+    loginUser({ name: "Dave" });
+  };
+
+  const clearStatus = () => {
+    loginUser(null);
+  };
+
+  return (
+    <div>
+      <div>Home Page</div>
+      <div>
+        <button onClick={checkStatus}>Check Status</button>
+      </div>
+      <div>
+        <button onClick={updateStatus}>Update Status</button>
+      </div>
+      <div>
+        <button onClick={clearStatus}>Clear Status</button>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
