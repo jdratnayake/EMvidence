0f63ae047c3fb6de75716340318d6741a039d838
DinilR <dinilsratnayake@gmail.com>
2024-01-08 12:17:42 +0530

    Frontend::Preprocess Interface

--- frontend/src/pages/AnalysisPageModified/AnalysisPage1.js
+++ frontend/src/pages/AnalysisPageModified/AnalysisPage1.js
@@ -6,6 +6,11 @@ import {
   Button,
   Container,
   CssBaseline,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  NativeSelect,
+  Select,
   Step,
   StepConnector,
   StepLabel,
@@ -16,16 +21,64 @@ import {
 import { styled } from "@mui/system";
 
 const ComponentOuter = styled(Container)(() => ({
-  marginTop: "50px",
+  marginTop: "20px",
 }));
 
 const StepperBox = styled(Box)(() => ({
   backgroundColor: "#C0C0C0",
+  height: "70vh"
 }));
 
 const ContainerBox = styled(Box)(() => ({
   marginLeft: "5%",
-  marginTop: "3%",
+  marginTop: "1%",
+  
+}));
+
+const ContentSectionBox = styled(Box)(() => ({
+  height: "55vh"
+}));
+
+const FormElementBox = styled(Box)(() => ({
+  display: "flex",
+  justifyContent: "flex-start",
+  marginTop: "20px",
+  marginLeft: "20%",
+  bottom: "10px"
+}));
+
+const SelectBox = styled(Box)(() => ({
+  marginLeft: "10%",
+  width: "500px",
+}));
+
+const BottomButtonBox = styled(Box)(() => ({
+  marginTop: "10%",
+}));
+
+const SelectLabel = styled(Typography)(() => ({
+  marginTop: "5%",
+}));
+
+const Selector = styled(Select)(() => ({
+  width: "100%",
+  boxShadow: 'none', '.MuiOutlinedInput-notchedOutline': { border: 0 },
+  background: "#00225640",
+  color: "#FFFFFF"
+}));
+
+const LabelBox = styled(Box)(() => ({
+  width:"150px"
+}));
+
+const ButtonBottomLeft = styled(Button)(() => ({
+  color:"#00245A",
+  marginLeft:"5px"
+}));
+
+const ButtonBottomRight = styled(Button)(() => ({
+  marginRight:"5px",
+  backgroundColor: "#00245A"
 }));
 
 const steps = [
@@ -62,6 +115,27 @@ const QontoConnector = styled(StepConnector)(({ theme }) => ({
 export default function AnalysisPage1() {
   const [activeStep, setActiveStep] = useState(0);
   const [skipped, setSkipped] = useState(new Set());
+  const [downSampleRate, setDownSampleRate] = useState(0);
+  const [domainConverson, setDomainConversion] = useState(0);
+  const [freqSelection, setFreqSelection] = useState(0);
+  const [sampleSelection,setSampleSelection] = useState(0)
+
+  const handleChangeDownSampling = (event) => {
+    console.log(event);
+    setDownSampleRate(event.target.value);
+  };
+
+  const handleDomainConversion = (event) => {
+    setDomainConversion(event.target.value);
+  };
+
+  const handleFreqSelection = (event) => {
+    setFreqSelection(event.target.value);
+  };
+
+  const handleSampleSelection = (event) => {
+    setSampleSelection(event.target.value);
+  }
 
   const isStepOptional = (step) => {
     return step === 1;
@@ -123,7 +197,7 @@ export default function AnalysisPage1() {
         </Typography>
         <ComponentOuter maxWidth="lg">
           <StepperBox>
-            <Stepper activeStep={activeStep} connector={<QontoConnector/>}>
+            <Stepper activeStep={activeStep} connector={<QontoConnector />}>
               {steps.map((label, index) => {
                 const stepProps = {};
                 const labelProps = {};
@@ -142,6 +216,7 @@ export default function AnalysisPage1() {
                 );
               })}
             </Stepper>
+
             {activeStep === steps.length ? (
               <React.Fragment>
                 <Typography sx={{ mt: 2, mb: 1 }}>
@@ -150,6 +225,107 @@ export default function AnalysisPage1() {
                 <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
                   <Box sx={{ flex: "1 1 auto" }} />
                   <Button onClick={handleReset}>Reset</Button>
+                </Box>
+              </React.Fragment>
+            ) : activeStep === 1 ? (
+              <React.Fragment>
+
+                <ContentSectionBox>
+                  <FormControl fullWidth>
+                    <FormElementBox>
+                      <LabelBox>
+                      <SelectLabel variant="body1" display="block">
+                        DownSampling:
+                      </SelectLabel>
+                      </LabelBox>
+                      <SelectBox>
+                        <Selector
+                          defaultValue={1}
+                          value={downSampleRate}
+                          onChange={handleChangeDownSampling}
+                          displayEmpty
+                          inputProps={{ "aria-label": "without label" }}
+                        >
+                          <MenuItem value={0}>Not Downsampled</MenuItem>
+                          <MenuItem value={10}>To 10MHz</MenuItem>
+                          <MenuItem value={8}>To 8MHz</MenuItem>
+                          <MenuItem value={4}>To 4MHz</MenuItem>
+                        </Selector>
+                      </SelectBox>
+                    </FormElementBox>
+                    <FormElementBox>
+                      <LabelBox>
+                      <SelectLabel variant="body1" display="block">
+                        Domain Conversion:
+                      </SelectLabel>
+                      </LabelBox>
+                      <SelectBox>
+                        <Selector
+                          defaultValue={1}
+                          value={domainConverson}
+                          onChange={handleDomainConversion}
+                          displayEmpty
+                          inputProps={{ "aria-label": "without label" }}
+                        >
+                          <MenuItem value={0}>STFT (FFT_SIZE = 2048 & Overlap Size = 256)</MenuItem>
+                          <MenuItem value={10}>STFT (FFT_SIZE = 1024 & Overlap Size = 256)</MenuItem>
+                          <MenuItem value={8}>FFT</MenuItem>
+                        </Selector>
+                      </SelectBox>
+                    </FormElementBox>
+                    <FormElementBox>
+                      <LabelBox>
+                      <SelectLabel variant="body1" display="block">
+                        Sanmple Selection:
+                      </SelectLabel>
+                      </LabelBox>
+                      <SelectBox>
+                        <Selector
+                          defaultValue={1}
+                          value={sampleSelection}
+                          onChange={handleSampleSelection}
+                          displayEmpty
+                          inputProps={{ "aria-label": "without label" }}
+                        >
+                          <MenuItem value={0}>All Samples</MenuItem>
+                          <MenuItem value={1}>First 20k Samples</MenuItem>
+                          <MenuItem value={2}>.25 to .75 of samples</MenuItem>
+                        </Selector>
+                      </SelectBox>
+                    </FormElementBox>
+                    <FormElementBox>
+                      <LabelBox>
+                      <SelectLabel variant="body1" display="block">
+                        Frequency Channel Selection:
+                      </SelectLabel>
+                      </LabelBox>
+                      <SelectBox>
+                        <Selector
+                          defaultValue={1}
+                          value={freqSelection}
+                          onChange={handleFreqSelection}
+                          displayEmpty
+                          inputProps={{ "aria-label": "without label" }}
+                        >
+                          <MenuItem value={0}>All Channels</MenuItem>
+                          <MenuItem value={1}>Highest variance of averaged elements</MenuItem>
+                          <MenuItem value={2}>Other</MenuItem>
+                        </Selector>
+                      </SelectBox>
+                    </FormElementBox>
+                  </FormControl>
+                </ContentSectionBox>
+                <Box sx={{ display: "flex", flexDirection: "row", pt2: 2 }}>
+                  <ButtonBottomLeft
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    sx={{ mr: 1 }}
+                    variant="text"
+                  >
+                    Back
+                  </ButtonBottomLeft>
+                  <Box sx={{ flex: "1 1 auto" }} />
+                  <ButtonBottomRight variant="contained" onClick={handleNext}>Pre-Process</ButtonBottomRight>
                 </Box>
               </React.Fragment>
             ) : (
@@ -168,12 +344,16 @@ export default function AnalysisPage1() {
                   </Button>
                   <Box sx={{ flex: "1 1 auto" }} />
                   {isStepOptional(activeStep) && (
-                  <Button color="inherit" conClick={handleSkip} sx={{mr:1}}>
-                    Skip
-                  </Button>
+                    <Button
+                      color="inherit"
+                      conClick={handleSkip}
+                      sx={{ mr: 1 }}
+                    >
+                      Skip
+                    </Button>
                   )}
                   <Button onClick={handleNext}>
-                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    {activeStep === steps.length - 1 ? "Finish" : "Next"}
                   </Button>
                 </Box>
               </React.Fragment>
