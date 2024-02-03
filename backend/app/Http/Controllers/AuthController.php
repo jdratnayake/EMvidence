8cc5f859140ebb5fa37575d7010c95d87252b193
Janitha Ratnayake <62105403+jdratnayake@users.noreply.github.com>
2024-02-03 11:29:23 +0530

    revert to previous commit

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -12,16 +12,6 @@ use \Firebase\JWT\JWT;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api', ['except' => ['login','register']]);
-    // }
-
     // Generate JWT token for the user
     private function generateJWTToken($user)
     {
@@ -154,51 +144,4 @@ class AuthController extends Controller
 
     }
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    // public function me()
-    // {
-    //     return response()->json(auth()->user());
-    // }
-
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
-    {
-        // auth()->logout();
-
-        // return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    // public function refresh()
-    // {
-    //     return $this->respondWithToken(auth()->refresh());
-    // }
-
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    // protected function respondWithToken($token)
-    // {
-    //     return response()->json([
-    //         'access_token' => $token,
-    //         'token_type' => 'bearer',
-    //         'expires_in' => auth()->factory()->getTTL() * 60
-    //     ]);
-    // }
 }
--- frontend/src/pages/SignInPage/SignInPage.js
+++ frontend/src/pages/SignInPage/SignInPage.js
@@ -10,100 +10,47 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 import TextBox from "../../components/TextBox/TextBox";
 import Copyright from "../../components/Copyright/Copyright";
 import { validateEmail, validatePassword } from "./Validation";
-import TextField from '@mui/material/TextField';
-import axios from "axios";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
-import { useUser } from "../../contexts/UserContext";
-import { useNavigate } from 'react-router-dom';
-
-const baseURL = 'http://127.0.0.1:8000/api/login';
 
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-
-  const navigate = useNavigate();
-  const { user, loginUser } = useUser();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-
-  };
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
-    if (email != "" && password != "") {
-      
-      axios.post(baseURL, {
-        email: email,
-        password: password
-      })
-        .then((response) => {
-          if (response.data.status == 200){
-            loginUser({ 
-              first_name : response.data.user.first_name, 
-              last_name : response.data.user.last_name, 
-              role : response.data.user.role, 
-              user_id : response.data.user.user_id,
-              token : response.data.token,
-              email : response.data.user.email, 
+    const data = new FormData(event.currentTarget);
 
-            });
-           
-            if (response.data.user.role === "admin"){
-              navigate('/home_admin');
-            } else if (response.data.user.role === "developer"){
-              navigate('/home_dev');
-              
-            }else if (response.data.user.role === "invesigator"){
-              navigate('/home_investigator');
-              
-            }
-              
-          }else if (response.data.status == 401){
-            alert('Email or Password is invalid')
-          } else {
-            alert('Internel server error')
-          }
-          
-        })
-        .catch((error) => {
-          // Handle errors related to the HTTP request
-          console.error("Error making the request:", error);
-          alert('Failed to connect to the server. Please try again later.');
-        });
-    } else {
-      alert("Email or Password is empty.")
+    // Reset error messages
+    setEmailError("");
+    setPasswordError("");
+
+    let isValid = true;
+
+    if (!validateEmail(data.get("email"))) {
+      setEmailError("Invalid email address");
+      isValid = false;
     }
 
+    const passwordValidationResult = validatePassword(data.get("password"));
+    if (passwordValidationResult !== true) {
+      setPasswordError(passwordValidationResult);
+      isValid = false;
+    }
+
+    if (isValid) {
+      console.log("Form data is valid");
+      console.log({
+        email: data.get("email"),
+        password: data.get("password"),
+      });
+    } else {
+      console.log("Form data is invalid");
+    }
   };
 
   return (
@@ -131,49 +78,31 @@ export default function SignIn() {
               noValidate
               sx={{ mt: 1 }}
             >
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  onChange={handleEmail}
-                />
-              </Grid>
-              <br />
-              <Grid item xs={12}>
-                <FormControl fullWidth variant="outlined" required>
-                  <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                  <OutlinedInput
-                    id="password"
-                    type={showPassword ? 'text' : 'password'}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                    label="Password"
-                    onChange={handlePassword}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid container justifyContent="flex-end">
-                <Grid>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-                </Grid>
-                
-              </Grid>
+              <TextBox
+                idValue="email"
+                labelValue="Email Address"
+                nameValue="email"
+                autoCompleteValue="email"
+                autoFocusStatus={true}
+                errorStatus={emailError !== ""}
+                helperTextValue={emailError}
+              />
+
+              <TextBox
+                idValue="password"
+                labelValue="Password"
+                typeName="password"
+                nameValue="password"
+                autoCompleteValue="current-password"
+                autoFocusStatus={true}
+                errorStatus={passwordError !== ""}
+                helperTextValue={passwordError}
+              />
+
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
               <Button
                 type="submit"
                 fullWidth
@@ -182,12 +111,18 @@ export default function SignIn() {
               >
                 Sign In
               </Button>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-
             </Box>
           </Box>
           <Copyright />
