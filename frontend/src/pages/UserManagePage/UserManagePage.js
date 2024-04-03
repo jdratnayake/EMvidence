f98959785ef492aa3497db8d6b600f52abe4005d
Senal Punsara <kksenalpunsara@gmail.com>
2024-04-03 23:53:58 +0530

    modified user manage ui

--- frontend/src/pages/UserManagePage/UserManagePage.js
+++ frontend/src/pages/UserManagePage/UserManagePage.js
@@ -36,12 +36,6 @@ import axios from "axios";
 import DeactivateModal from "../../components/DeactivateModal/DeactivateModal";
 import ActivateModal from "../../components/ActivateModal/ActivateModal";
 
-const columns = [
-  { id: "name", label: "Name", midWidth: 200 },
-  { id: "status", label: "Status", midWidth: 150 },
-  { id: "actions", label: "Actions", midWidth: 300 },
-];
-
 const sxStyle = {
   "&:hover": {
     "&& fieldset": {
@@ -110,60 +104,9 @@ const TableBox = styled(Box)(() => ({
   width: "100%",
 }));
 
-const SearchButton = styled(Button)(() => ({
-  backgroundColor: "#00245A",
-  height: "60px",
-  borderRadius: "0",
-}));
-
-const SearchField = styled(TextField)(() => ({
-  border: "2px solid #00245A",
-  // MUI element css change
-  "& label.Mui-focused": {
-    color: "transparent",
-  },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "transparent",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "transparent",
-    },
-    "&:hover fieldset": {
-      borderColor: "transparent",
-    },
-    "&.Mui-focused fieldset": {
-      borderColor: "transparent",
-    },
-  },
-  width: "40vw",
-}));
-
-const TableHeadRow = styled(TableRow)(() => ({
-  "& th": {
-    color: "rgba(96, 96, 96)",
-    backgroundColor: "#FCFCFD",
-  },
-}));
-
-const TableCellBlue = styled(TableCell)(() => ({
-  color: "#00245A",
-}));
-
-// const SearchField = styled(TextField)(() => ({
-//   color: 'var(--text-color, #00245A)',
-//   border: '2px solid #00245A', // Set border color and thickness
-//   borderRadius: '4px', // Add optional rounded corners
-//   '& .MuiOutlinedInput-root': { // Target outlined variant properties
-//     '& fieldset': {
-//       borderColor: '#00245A', // Ensure consistency across variants
-//     },
-//   },
-// }));
 
 function UserManagePage() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+ 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isActivateModalOpen, setIsActivateModalOpen] = useState(false);
   const [deactivateUserId, setDeactivateUserId] = useState(null);
@@ -176,9 +119,6 @@ function UserManagePage() {
   const handleClose = () => setIsModalOpen(false);
   const handleActivateClose = () => setIsActivateModalOpen(false);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
 
   const handleClicked = (userId) => {
     setDeactivateUserId(userId);
@@ -192,10 +132,6 @@ function UserManagePage() {
 
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   const rows = [
     { id: 1, name: "User 1", status: "Inactive", actions: "1" },
@@ -212,92 +148,7 @@ function UserManagePage() {
     return null;
   }
 
-  function getActions(value) {
-    return (
-      <TableCell align="center">
-        <Box sx={{ display: "flex", flexDirection: "row", justifyContent:"center", alignItems:"center"}}>
-          <Button
-            variant="outlined"
-            sx={{
-              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
-              '&:hover': {
-                borderColor: "#00245A", // Change to the desired hover color
-              },
-            }}
-            onClick={() => { }}
-          >
-            <ModeEditIcon sx={{ ml: -1, mr: 1 }} />
-            Edit
-          </Button>
-          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
-          <Button
-            variant="outlined"
-            sx={{
-              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
-              '&:hover': {
-                borderColor: "#00245A", // Change to the desired hover color
-              },
-            }}
-            onClick={() => { }}
-          >
-            <VisibilityIcon sx={{ ml: -1, mr: 1 }} />
-            View
-          </Button>
-          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
-          {getStatusByActions(value) === "Active" ? (
-            <Button
-              variant="outlined"
-              color="error"
-              onClick={() => handleClicked(value)}
-            >
-              {/* <DeleteIcon sx={{ ml: -1, mr: 1 }} /> */}
-              Deactivate
-            </Button>
-          ) : (
-            <Button
-              variant="outlined"
-              sx={{
-                color: "green", cursor: "pointer", borderColor: "rgba(0, 128, 0, 0.4)",  pl:3, pr:3,
-                '&:hover': {
-                  borderColor: "green",
-                },
-              }}
-              onClick={() => handleActivateClicked(value)}
-            >
-              {/* <DeleteIcon sx={{ ml: -1, mr: 1 }} /> */}
-              Activate
-            </Button>
-          )}
-        </Box>
-      </TableCell>
-    );
-  }
 
-  function getStatus(value) {
-    if (value === "Active") {
-      return (
-        <TableCell align="center">
-          <Chip
-            sx={{ background: "#ECFDF3", color: "#037847", mt: "10px" }}
-            label={value}
-          />
-        </TableCell>
-      );
-    } else {
-      return (
-        <TableCell  align="center">
-          <Chip
-            sx={{ background: "#FFF2F2", color: "red", mt: "10px" }}
-            label={value}
-          />
-        </TableCell>
-      );
-    }
-  }
-
-  // for the table
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
   function TablePaginationActions(props) {
     const theme = useTheme();
 
@@ -389,7 +240,7 @@ function UserManagePage() {
             value={searchText}
             onChange={handleSearch}
             variant="outlined"
-            style={{ width: "500px", marginTop: "40px", backgroundColor: "white" }}
+            style={{ width: "500px", marginTop: "40px", backgroundColor: "white", borderRadius: 4 }}
             InputProps={{
               endAdornment: <SearchIcon sx={{ fontSize: 30 }} />,
             }}
@@ -423,47 +274,222 @@ function UserManagePage() {
                     </TableRow>
                   </TableHead>
                   <TableBody>
-                    {rows
-                      .slice(
-                        page * rowsPerPage,
-                        page * rowsPerPage + rowsPerPage
-                      )
-                      .map((row) => {
-                        return (
-                          <TableRow
-                            hover
-                            role="checkbox"
-                            tableIndex={-1}
-                            key={row.code}
+                    <TableRow hover={true}>
+                      <TableCell >
+                        User 1
+                      </TableCell>
+                      <TableCell align="center">
+                        <Chip
+                          sx={{ background: "#ECFDF3", color: "#037847", mt: "10px" }}
+                          label={"Active"}
+                        />
+                      </TableCell>
+                      <TableCell align="center">
+                        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A",
+                              },
+                            }}
+                            onClick={() => { }}
                           >
-                            {columns.map((column) => {
-                              const value = row[column.id];
-                              // console.log(column.id);
-                              return column.id === "status" ? (
-                                getStatus(value)
-                              ) : column.id === "actions" ? (
-                                getActions(value)
-                              ) : (
-                                <TableCell key={column.id}>
-                                  {value}
-                                </TableCell>
-                              );
-                            })}
-                          </TableRow>
-                        );
-                      })}
+                            <ModeEditIcon sx={{ ml: -1, mr: 1 }} />
+                            Edit
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A", // Change to the desired hover color
+                              },
+                            }}
+                            onClick={() => { }}
+                          >
+                            <VisibilityIcon sx={{ ml: -1, mr: 1 }} />
+                            View
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+
+                          <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => { handleClicked(1) }}
+                          >
+                            {/* <DeleteIcon sx={{ ml: -1, mr: 1 }} /> */}
+                            Deactivate
+                          </Button>
+                        </Box>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow hover={true}>
+                      <TableCell >
+                        User 2
+                      </TableCell>
+                      <TableCell align="center">
+                        <Chip
+                          sx={{ background: "#ECFDF3", color: "#037847", mt: "10px" }}
+                          label={"Active"}
+                        />
+                      </TableCell>
+                      <TableCell align="center">
+                        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A",
+                              },
+                            }}
+                            onClick={() => { }}
+                          >
+                            <ModeEditIcon sx={{ ml: -1, mr: 1 }} />
+                            Edit
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A", // Change to the desired hover color
+                              },
+                            }}
+                            onClick={() => { }}
+                          >
+                            <VisibilityIcon sx={{ ml: -1, mr: 1 }} />
+                            View
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+
+                          <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => { handleClicked(2) }}
+                          >
+                            {/* <DeleteIcon sx={{ ml: -1, mr: 1 }} /> */}
+                            Deactivate
+                          </Button>
+                        </Box>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow hover={true}>
+                      <TableCell >
+                        User 3
+                      </TableCell>
+                      <TableCell align="center">
+                        <Chip
+                          sx={{ background: "#FFF2F2", color: "red", mt: "10px" }}
+                          label={"Inactive"}
+                        />
+                      </TableCell>
+                      <TableCell align="center">
+                        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A",
+                              },
+                            }}
+                            onClick={() => { }}
+                          >
+                            <ModeEditIcon sx={{ ml: -1, mr: 1 }} />
+                            Edit
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A", // Change to the desired hover color
+                              },
+                            }}
+                            onClick={() => { }}
+                          >
+                            <VisibilityIcon sx={{ ml: -1, mr: 1 }} />
+                            View
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+
+                          <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => { handleClicked(3) }}
+                          >
+                            {/* <DeleteIcon sx={{ ml: -1, mr: 1 }} /> */}
+                            Deactivate
+                          </Button>
+                        </Box>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow hover={true}>
+                      <TableCell >
+                        User 4
+                      </TableCell>
+                      <TableCell align="center">
+                        <Chip
+                          sx={{ background: "#FFF2F2", color: "red", mt: "10px" }}
+                          label={"Inactive"}
+                        />
+                      </TableCell>
+                      <TableCell align="center">
+                        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A",
+                              },
+                            }}
+                            onClick={() => { }}
+                          >
+                            <ModeEditIcon sx={{ ml: -1, mr: 1 }} />
+                            Edit
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "#00245A", cursor: "pointer", borderColor: "rgba(0, 36, 90, 0.4)",
+                              '&:hover': {
+                                borderColor: "#00245A", // Change to the desired hover color
+                              },
+                            }}
+                            onClick={() => { }}
+                          >
+                            <VisibilityIcon sx={{ ml: -1, mr: 1 }} />
+                            View
+                          </Button>
+                          <span style={{ marginLeft: "10px" }}>{"\u00A0"}</span>
+
+                          <Button
+                            variant="outlined"
+                            sx={{
+                              color: "green", cursor: "pointer", borderColor: "rgba(0, 128, 0, 0.4)", pl: 3, pr: 3,
+                              '&:hover': {
+                                borderColor: "green",
+                              },
+                            }}
+                            onClick={() => handleActivateClicked(4)}
+                          >
+                            {/* <DeleteIcon sx={{ ml: -1, mr: 1 }} /> */}
+                            Activate
+                          </Button>
+                        </Box>
+                      </TableCell>
+                    </TableRow>
                   </TableBody>
                 </Table>
               </TableContainer>
-              <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
             </Paper>
           </TableBox>
         </ContentBox>
