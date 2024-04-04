a65ba24828019b06f8897e39b93a0d070fd2dfe0
Senal Punsara <kksenalpunsara@gmail.com>
2024-04-04 20:25:34 +0530

    modified admin page ui

--- frontend/src/components/DashboardCard/DashboardCard.js
+++ frontend/src/components/DashboardCard/DashboardCard.js
@@ -1,14 +1,17 @@
 import React, { cloneElement } from "react";
 import { Typography, CardContent, Card } from "@mui/material";
 import "./DashboardCard.css";
+import { borderRadius, Box, Container, minWidth, styled } from "@mui/system";
+
 
 function DashboardCard({ icon, title, count }) {
-  const gray_for_number = "#D4D4D4";
+  const gray_for_number = "black";
   const summary_icon_size = "40px";
   const card_component_properties = {
-    width: "250px",
-    height: "200px",
-    borderRadius: "16px",
+    width: "100%",
+    height: "150px",
+    borderRadius: "10px",
+    minWidth:"250px"
   };
 
   return (
@@ -21,18 +24,21 @@ function DashboardCard({ icon, title, count }) {
             height: "100%",
           }}
         >
-          <div className="card-content-container">
-            <div className="icon">
-              {cloneElement(icon, { style: { fontSize: summary_icon_size } })}
+          <div className="card-content-container" style={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
+            <div className="icon_and_title" style={{ display: "flex", justifyContent: "center" }}>
+              <div className="icon">
+                {cloneElement(icon, { style: { fontSize: summary_icon_size } })}
+              </div>
+              <div className="title" style={{ marginTop: "5px", marginLeft: "6px" }}>
+                <Typography variant="h5" component="div">
+                  {title}
+                </Typography>
+              </div>
             </div>
-            <div className="title">
-              <Typography variant="h5" component="div">
-                {title}
-              </Typography>
-            </div>
-            <div className="count">
+
+            <div className="count"  style={{ display: "flex", justifyContent: "center", paddingTop: "8px"}} >
               <Typography
-                variant="h6"
+                variant="h4"
                 component="div"
                 style={{ color: gray_for_number }}
               >
